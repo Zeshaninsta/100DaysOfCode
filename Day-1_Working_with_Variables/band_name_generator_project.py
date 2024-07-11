fcdf6180c2c30bc575b9f52bc8484efa7e0a98a2
zeshaninsta <zeshaninsta@gmail.com>
2024-07-11 18:22:06 +0300

    added First Project of working with Variables

---
+++ Day-1_Working_with_Variables/band_name_generator_project.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+print("what's name of the city you grew up in?")
+city = input()
+print("what's your pet's name?")
+pet = input()
+print(f"Your band name could be {city} {pet}\n")
